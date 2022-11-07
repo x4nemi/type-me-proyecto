05386d4cecc2ab5e8e123272bbdb0336564f3958
x4nemi <ximena.garcia7482@alumnos.udg.mx>
2022-11-06 19:14:27 -0600

    bug fixes

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -1,23 +1,8 @@
 import { ImSun } from "react-icons/im";
 import { FaRegMoon } from "react-icons/fa";
-import { useDispatch, useSelector } from "react-redux";
-import { setTheme } from "../store/slices/theme/themeSlice";
 import { useEffect, useState } from "react";
 
 export const ThemeButton = () => {
-    // const { theme } = useSelector((state) => state.theme);
-    // const dispatch = useDispatch();
-    // const handleThemeChange = () => {
-    //     dispatch(setTheme(theme === "night" ? "winter" : "night"));
-    // };
-    // useEffect(() => {
-    //     if (theme === "winter") {
-    //         document.documentElement.setAttribute("data-theme", "winter");
-    //     } else {
-    //         document.documentElement.setAttribute("data-theme", "night");
-    //     }
-    // }, [theme]);
-
     const [darkMode, setDarkMode] = useState(false);
 
     const themeToggler = () => {
